037f5690dbec96adde1aad8c8f3b1b4a03dcefd0
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-18 19:10:35 +0330

    adding customer route and controllers

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\admin\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group([],function(){
-    Route::get('/dashboard', fn()=> view('admin.dashboard.index'));
+    Route::get('/dashboard', fn()=> view('admin.dashboard.index'))->name('dashboard');
+
+   Route::resource('customers', CustomerController::class);
 });
 
