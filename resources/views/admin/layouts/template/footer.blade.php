0e9a1deb278f4b6310ad2fb9e63cd5d6500a2e87
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-19 07:27:17 +0330

    change style

--- resources/views/admin/layouts/template/footer.blade.php
+++ resources/views/admin/layouts/template/footer.blade.php
@@ -2,15 +2,6 @@
     <div class="container-fluid">
         <div class="row text-muted">
             <div class="col-6 text-start">
-                <p class="mb-0">
-                    <a class="text-muted" rel="nofollow" href="https://talarkadeh.com"
-                        target="_blank"><strong>Talarkadeh</strong></a> -
-                    <a class="text-muted" href="https://talarkadeh.com" target="_blank">
-                        <strong>Hesabnegar</strong>
-                    </a> &copy;
-                </p>
-            </div>
-            <div class="col-6 text-end">
                 <ul class="list-inline">
                     <li class="list-inline-item">
                         <a class="text-muted" href="mailto:pouria.782@gmail.com">پشتیبانی</a>
@@ -22,6 +13,15 @@
                         <a class="text-muted" href="mailto:pouria.782@gmail.com">حریم شخصی</a>
                     </li>
                 </ul>
+            </div>
+            <div class="col-6 text-end">
+                <p class="mb-0">
+                    <a class="text-muted" rel="nofollow" href="https://talarkadeh.com"
+                        target="_blank"><strong>Talarkadeh</strong></a> -
+                    <a class="text-muted" href="https://talarkadeh.com" target="_blank">
+                        <strong>Hesabnegar</strong>
+                    </a> &copy;
+                </p>
             </div>
         </div>
     </div>
