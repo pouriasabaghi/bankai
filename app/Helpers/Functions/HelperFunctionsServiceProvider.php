321fecf15fa7e6a413a3ccc0b6fc535caf360d9f
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-20 15:03:23 +0330

    fix bug function directory was capital

--- app/Helpers/Functions/HelperFunctionsServiceProvider.php
+++ app/Helpers/Functions/HelperFunctionsServiceProvider.php
@@ -8,7 +8,7 @@ class HelperFunctionsServiceProvider extends ServiceProvider{
 
     public function register()
     {
-        $file = app_path('Helpers/functions/HelperFunctions.php');
+        $file = app_path('Helpers/Functions/HelperFunctions.php');
         if (file_exists($file)) {
             require_once($file);
         }
