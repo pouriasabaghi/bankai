0a674d2859c38c5fdbb7dd5ddf0ca6813037cffd
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-21 10:15:05 +0330

    install livewire

--- app/Http/Controllers/admin/ContractController.php
+++ app/Http/Controllers/admin/ContractController.php
@@ -4,13 +4,28 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Contract;
+use App\Services\ContractService;
 use Illuminate\Http\Request;
 
 class ContractController extends Controller
 {
+    protected ContractService $service ;
+
+    public function __construct()
+    {
+        $this->service = new ContractService();
+    }
+
     public function index()
     {
         $contracts = Contract::query()->latest()->paginate(50);
         return view('admin.contracts.index', compact('contracts'));
+    }
+
+    public function create()
+    {
+        $service = $this->service;
+        $formAttributes = $service->formAttributes();
+        return view('admin.contracts.create', compact('formAttributes'));
     }
 }
