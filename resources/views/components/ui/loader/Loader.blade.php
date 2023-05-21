b61c4defcce515a80b7cada4666af5083b891759
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-21 15:26:13 +0330

    add loader component

---
+++ resources/views/components/ui/loader/Loader.blade.php
@@ -0,0 +1,6 @@
+<div class="loader" {{ $livewire ?? '' }}>
+    <div   class="spinner-{{ $type ?? 'border' }}
+                  spinner-{{ $type ?? 'border' }}-sm text-{{ $color ?? 'primary' }}
+                  {{ $class ?? '' }}">
+    </div>
+</div>
