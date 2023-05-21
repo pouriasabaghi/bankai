8ef636a4d60330de6622f405947560963129d03d
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-21 08:58:08 +0330

    add contracts model and migration

---
+++ app/Models/admin/Contract.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['customer_id', 'company_id', 'name', 'desc', 'financial_status', 'contract_status', 'total_price', 'type', 'contract_number', 'period', 'signed_at', 'canceled_at'];
+}
---
+++ database/migrations/2023_05_21_094226_create_contracts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('customer_id');
+            $table->foreignId('company_id');
+
+            $table->string('name')->nullable();
+            $table->text('desc')->nullable();
+            $table->enum('financial_status', ['paid', 'billed']);
+            $table->enum('contract_status', ['progress', 'finished', 'canceled', 'renewal']);
+            $table->text('total_price')->nullable();
+            $table->string('type')->nullable();
+            $table->string('contract_number')->nullable() ;
+            $table->string('period')->nullable();
+            $table->dateTime('signed_at')->nullable();
+            $table->dateTime('canceled_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
