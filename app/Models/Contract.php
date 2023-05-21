41b528a5102f15c973148de77ed982ea87a62648
pouriasabaghi <pouriasabaghi@gmail.com>
2023-05-21 09:35:08 +0330

    add customer and company relation ship

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -10,4 +10,15 @@ class Contract extends Model
     use HasFactory;
 
     protected $fillable = ['customer_id', 'company_id', 'name', 'desc', 'financial_status', 'contract_status', 'total_price', 'type', 'contract_number', 'period', 'signed_at', 'canceled_at'];
+
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
