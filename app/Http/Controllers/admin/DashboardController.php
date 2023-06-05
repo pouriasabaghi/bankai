0cc7b61dc1086cfa73fb7d96661f6d1e7e9d3135
pouriasabaghi <pouriasabaghi@gmail.com>
2023-06-05 09:06:07 +0330

    remove unused

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Installment;
-use App\Notifications\InstallmentNotification;
-use App\Services\InstallmentService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Notification;
 
 class DashboardController extends Controller
 {
@@ -15,13 +12,6 @@ class DashboardController extends Controller
     public function index()
     {
         $debtorInstallments = Installment::query()->where('due_at', '<=', now())->where('status', 'billed')->with('contract')->get();
-
-        $debtorInstallments->each(function($installment, $key){
-            Notification::send($installment,  new InstallmentNotification($installment));
-        });
-
-
-        dd('okk');
         return view('admin.dashboard.index', compact('debtorInstallments'));
     }
 }
