c2ff3857f316cd0b77350f396980b5953937edf5
pouriasabaghi <pouriasabaghi@gmail.com>
2023-07-08 17:23:46 +0330

    add advance payment to overal detail of contract

--- resources/views/admin/receives/layouts/detail.blade.php
+++ resources/views/admin/receives/layouts/detail.blade.php
@@ -105,6 +105,15 @@
                 </div>
             </div>
 
+             <div class="row mx-0 px-0 border-2 border-bottom py-3 ">
+                <div class="col-6">
+                    <span>پیش قرارداد:</span>
+                </div>
+                <div class="col-6">
+                    <span> {{ $contract->advance_payment_str }} </span>
+                    <small>تومان</small>
+                </div>
+            </div>
 
             <div class="row mx-0 px-0 border-2 border-bottom py-3">
                 <div class="col-6 ">
