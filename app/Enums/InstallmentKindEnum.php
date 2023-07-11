35784fa324098e7c9c0c747962674687a195ed65
pouriasabaghi <pouriasabaghi@gmail.com>
2023-07-11 20:08:03 +0330

    add installment kind enum

---
+++ app/Enums/InstallmentKindEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace  App\Enums;
+
+
+
+enum InstallmentKindEnum: string
+{
+    case Deposit = 'deposit';
+    case Check = 'check';
+
+
+    public function toString()
+    {
+        return match ($this) {
+            self::Deposit => 'واریز',
+            self::Check => 'چک',
+            default => 'ثبت نشده',
+        };
+    }
+}
